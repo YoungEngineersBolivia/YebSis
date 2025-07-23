e3b9512135d25144b2cf4c9560c452677264acae
jdanigh03 <danielguevarahoyos03@gmail.com>
2025-07-23 18:11:44 -0400

    feat/first-five-administrator-functions

--- resources/views/administrador/baseAdministrador.blade.php
+++ resources/views/administrador/baseAdministrador.blade.php
@@ -9,77 +9,90 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
 </head>
 <body class="font-sans antialiased">
-    
-    <div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px; min-height: 100vh;">
-        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-            <img src="{{ Vite::asset('resources/img/ES_logo-02.png') }}" alt="Logo YE Bolivia" width="250px" class="me-2">
-        </a>
-        <hr>
-        <ul class="nav nav-pills flex-column mb-auto">
-            <li>
-                <a href="#" class="nav-link link-dark d-flex align-items-center gap-2">
-                    <i class="bi bi-clipboard-data"></i>
-                    Dashboard
-                </a>
-            </li>
-            <li>
-                <a href="#" class="nav-link link-dark d-flex align-items-center gap-2">
-                    <i class="bi bi-journal-text"></i>
-                    Registros
-                </a>
-            </li>
-            <li>
-                <a href="#" class="nav-link link-dark d-flex align-items-center gap-2">
-                    <i class="bi bi-person-workspace"></i>
-                    Tutores
-                </a>
-            </li>
-            <li>
-                <a href="#" class="nav-link link-dark d-flex align-items-center gap-2">
-                    <i class="bi bi-people-fill"></i>
-                    Usuarios
-                </a>
-            </li>
-            <li>
-                <a href="#" class="nav-link link-dark d-flex align-items-center gap-2">
-                    <i class="bi bi-calendar-week"></i>
-                    Horarios
-                </a>
-            </li>
-            <li>
-                <a href="#" class="nav-link link-dark d-flex align-items-center gap-2">
-                    <i class="bi bi-person-fill"></i>
-                    Estudiantes
-                </a>
-            </li>
-            <li>
-                <a href="#" class="nav-link link-dark d-flex align-items-center gap-2">
-                    <i class="bi bi-send-fill"></i>
-                    Publicar y Notificar
-                </a>
-            </li>
-            <li>
-                <a href="#" class="nav-link link-dark d-flex align-items-center gap-2">
-                    <i class="bi bi-folder-fill"></i>
-                    Programas
-                </a>
-            </li>
-            <li>
-                <a href="#" class="nav-link link-dark d-flex align-items-center gap-2">
-                    <i class="bi bi-patch-check-fill"></i>
-                    Graduados
-                </a>
-            </li>
-        </ul>
-        <hr>
-        
-        <div>
-            <a href="#" class="d-flex align-items-center link-dark text-decoration-none gap-2">
-                <button type="button" class="btn btn-danger w-75">Cerrar sesión</button>
-                <img src="https://github.com/mdo.png" alt="perfil" width="32" height="32" class="rounded-circle">
+    <div class="d-flex">
+        <div class="d-flex flex-column flex-shrink-0 p-3 bg-light position-fixed" style="width: 280px; min-height: 100vh;">
+            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+                <img src="{{ Vite::asset('resources/img/ES_logo-02.png') }}" alt="Logo YE Bolivia" width="250px" class="me-2">
             </a>
+            <hr>
+            <ul class="nav nav-pills flex-column mb-auto">
+                <li>
+                    <a href="#" class="nav-link link-dark d-flex align-items-center gap-2">
+                        <i class="bi bi-clipboard-data"></i>
+                        Dashboard
+                    </a>
+                </li>
+                <li>
+                    <a href="/administrador/registrosAdministrador" class="nav-link link-dark d-flex align-items-center gap-2">
+                        <i class="bi bi-journal-text"></i>
+                        Registrar
+                    </a>
+                </li>
+                <li>
+                    <a href="/administrador/tutoresAdministrador" class="nav-link link-dark d-flex align-items-center gap-2">
+                        <i class="bi bi-person-workspace"></i>
+                        Tutores
+                    </a>
+                </li>
+                <li>
+                    <a href="/administrador/usuariosAdministrador" class="nav-link link-dark d-flex align-items-center gap-2">
+                        <i class="bi bi-people-fill"></i>
+                        Usuarios
+                    </a>
+                </li>
+                <li>
+                    <a href="/administrador/horariosAdministrador" class="nav-link link-dark d-flex align-items-center gap-2">
+                        <i class="bi bi-calendar-week"></i>
+                        Horarios
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link link-dark d-flex align-items-center gap-2">
+                        <i class="bi bi-person-fill"></i>
+                        Estudiantes
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link link-dark d-flex align-items-center gap-2">
+                        <i class="bi bi-send-fill"></i>
+                        Publicar y Notificar
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link link-dark d-flex align-items-center gap-2">
+                        <i class="bi bi-folder-fill"></i>
+                        Programas
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link link-dark d-flex align-items-center gap-2">
+                        <i class="bi bi-patch-check-fill"></i>
+                        Graduados
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link link-dark d-flex align-items-center gap-2">
+                        <i class="bi bi-cash-coin"></i>
+                        Pagos
+                    </a>
+                </li>
+            </ul>
+            <hr>
+            
+            <div>
+                <a href="#" class="d-flex align-items-center link-dark text-decoration-none gap-2">
+                    <button type="button" class="btn btn-danger w-75">Cerrar sesión</button>
+                    <img src="https://github.com/mdo.png" alt="perfil" width="32" height="32" class="rounded-circle">
+                </a>
+            </div>
+        </div>
+        <div class="flex-grow-1 p-4" style="margin-left: 280px;">
+            <div class="container-fluid p-4">
+                @yield('content')
+            </div>
         </div>
     </div>
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('/administrador/HomeAdministrador');
+    return view('/administrador/baseAdministrador');
 });
 
-Route::get('/administrador/inicioAdministrador', function () {
-    return view('/administrador/inicioAdministrador'); 
+Route::get('/administrador/registrosAdministrador', function () {
+    return view('/administrador/registrosAdministradores'); 
+});
+
+Route::get('/administrador/tutoresAdministrador', function () {
+    return view('/administrador/tutoresAdministrador'); 
+});
+
+Route::get('/administrador/usuariosAdministrador', function () {
+    return view('/administrador/usuariosAdministrador'); 
+});
+
+Route::get('/administrador/horariosAdministrador', function () {
+    return view('/administrador/horariosAdministrador'); 
 });
