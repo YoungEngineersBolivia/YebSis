42a0fd9aa8a861233af13685da2f0f17d132a7b2
Dante1128 <danteyupanqui2811@gmail.com>
2025-10-02 16:44:56 -0400

    fix/teacher

--- resources/views/profesor/homeProfesor.blade.php
+++ resources/views/profesor/homeProfesor.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aplicación Educativa</title>
+    <title>Aplicación Educativaaaaaaaaaaaaaaaaaaa</title>
     <style>
         * {
             margin: 0;
