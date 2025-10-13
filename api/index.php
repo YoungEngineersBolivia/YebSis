e7e5176a9ffd213eb0e4d3d535faebf247dab17d
Dante1128 <danteyupanqui2811@gmail.com>
2025-10-13 17:28:27 -0400

    realis:vercel

--- api/index.php
+++ api/index.php
@@ -1,5 +1,18 @@
 <?php
+// Muestra errores temporalmente
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-require __DIR__.'/../bootstrap/app.php';
+// Carga el autoloader de Composer
+require __DIR__ . '/../vendor/autoload.php';
+
+// Carga la aplicación de Laravel
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// Maneja la request
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+$response->send();
+$kernel->terminate($request, $response);
