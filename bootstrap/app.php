9eda87fb2be9327d5ee31a1b4190eff5d9df027f
Dante1128 <danteyupanqui2811@gmail.com>
2025-10-21 15:39:38 -0400

    docker

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
+    // ->useCachePath(base_path('bootstrap/cache'))   ← elimina o comenta esto
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -16,6 +17,4 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })
-    ->create()
-    ->useStoragePath(__DIR__.'/../storage')
-    ->useCachePath(__DIR__.'/../bootstrap/cache');
+    ->create();
