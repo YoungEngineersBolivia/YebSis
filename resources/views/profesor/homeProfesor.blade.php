150607a97d0b9fe60cd5ea55755173e3894e31bd
Dante1128 <danteyupanqui2811@gmail.com>
2025-10-27 09:13:56 -0400

    Rename Persona.php to correct case

--- resources/views/profesor/homeProfesor.blade.php
+++ resources/views/profesor/homeProfesor.blade.php
@@ -6,16 +6,18 @@
 
 @section('content')
     <main class="main-content">
-       <a href="{{ route('profesor.alumnos') }}">
-            <button class="menu-button evaluate">Alumnos</button>
-        </a>
+      <a href="{{ route('profesor.alumnos') }}">
+    <button class="menu-button assigned">Alumnos</button>
+    </a>
 
-
-        <a href="{{ route()}}"></a>
+    <a href="{{ route('profesor.inventario') }}">
         <button class="menu-button assigned" onclick="handleAssigned()">
             Inventario
         </button>
+    </a>
+
     </main>
 
 <script src="{{ auto_asset('js/profesor/homeProfesor.js') }}"></script>
 @endsection
+
