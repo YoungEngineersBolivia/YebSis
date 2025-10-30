99874bdcd8e030a918a67a11ee4adc2c2e0e21f2
Dante1128 <danteyupanqui2811@gmail.com>
2025-10-30 09:40:48 -0400

    realis/EstudianteForm

--- resources/views/administrador/baseAdministrador.blade.php
+++ resources/views/administrador/baseAdministrador.blade.php
@@ -4,8 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'YE Bolivia - Administrador')</title>
-    <link rel="stylesheet" href="{{ auto_asset('css/dashboard/app.css') }}">
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
     
